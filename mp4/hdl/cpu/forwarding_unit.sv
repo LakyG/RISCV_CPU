2dbbd9cd99300b03f119d00c7deb76a9889ad3e0
peter li <shifang2@illinois.edu>
2021-11-09 17:46:21 -0600

    fixed some problems

--- mp4/hdl/cpu/forwarding_unit.sv
+++ mp4/hdl/cpu/forwarding_unit.sv
@@ -8,6 +8,7 @@ module forwarding_unit (
     output forwardingmux_t forwardingmux1_sel,
     output forwardingmux_t forwardingmux2_sel,
 
+
     // MEM Stage
     input rv32i_reg EXMEM_rd,
     input logic EXMEM_load_reg,
@@ -21,20 +22,29 @@ module forwarding_unit (
         forwardingmux1_sel = forwardingmux_t'(alumux_out);
         forwardingmux2_sel = forwardingmux_t'(alumux_out);
 
+
         // MEM Stage Forwarding
         if (EXMEM_load_reg && (EXMEM_rd != '0)) begin
-           if (EXMEM_rd == IDEX_rs1) forwardingmux1_sel = forwardingmux_t'(mem_alu_out); 
-           if (EXMEM_rd == IDEX_rs2) forwardingmux2_sel = forwardingmux_t'(mem_alu_out);
+           if (EXMEM_rd == IDEX_rs1) begin
+                forwardingmux1_sel = forwardingmux_t'(mem_alu_out); 
+           end
+           if (EXMEM_rd == IDEX_rs2) begin
+                forwardingmux2_sel = forwardingmux_t'(mem_alu_out);
+           end
         end
 
         // WB Stage Forwarding
         if (MEMWB_load_reg && (MEMWB_rd != '0)) begin
-            if (~(EXMEM_load_reg && (EXMEM_rd != '0) && (EXMEM_rd != IDEX_rs1))) begin
-                if (MEMWB_rd == IDEX_rs1) forwardingmux1_sel = forwardingmux_t'(wb_regfile_mux);
+            if (~(EXMEM_load_reg && (EXMEM_rd != '0) && (EXMEM_rd == IDEX_rs1))) begin
+                if (MEMWB_rd == IDEX_rs1) begin
+                    forwardingmux1_sel = forwardingmux_t'(wb_regfile_mux);
+                end
             end
 
-            if (~(EXMEM_load_reg && (EXMEM_rd != '0) && (EXMEM_rd != IDEX_rs2))) begin
-                if (MEMWB_rd == IDEX_rs2) forwardingmux2_sel = forwardingmux_t'(wb_regfile_mux);
+            if (~(EXMEM_load_reg && (EXMEM_rd != '0) && (EXMEM_rd == IDEX_rs2))) begin
+                if (MEMWB_rd == IDEX_rs2) begin 
+                    forwardingmux2_sel = forwardingmux_t'(wb_regfile_mux);
+                end
             end
         end
     end
